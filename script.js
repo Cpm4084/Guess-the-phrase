4c631da930d5582fb6668c09ca87e2718f162a34
Cameron Morrissey <116815955+Cpm4084@users.noreply.github.com>
2022-11-08 14:30:45 +0000

    Added game screens

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+let colorlist = ['gold', 'yellow', 'turquoise', 'red'];
+let screen = 0;
+let button;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  if (screen == 0) {
+    startingScreen();
+  } else if (screen == 1) {
+    gameScreen();
+  } else if (screen == 2) {
+    endScreen();
+  }
+}
+
+function startingScreen() {
+  background(150,150,150);
+  fill(0,0,0);
+  button = createButton('1 Player');
+  button.position(width / 2, height / 2);
+  button.mousePressed(gameScreen);
+}
+
+function gameScreen() {
+  background(50,50,50);
+}
+
+function endScreen() {
+  background(100,50,50);
 }
